d284b2da4ee945f8b1cd308cc042db409bfadecb
Jesse <verghost@gmail.com>
2020-10-12 00:43:43 -0400

    Remove fluff and specify lua for style

--- js/vglib.js
+++ js/vglib.js
@@ -1,5 +1,4 @@
 // JS for verghost.com
-// This is a bodged static content generator
 var dq = (s) => {
 	return document.querySelector(s);
 }, dqa = (s) => {
@@ -7,47 +6,13 @@ var dq = (s) => {
 };
 
 var verghostjs = {};
-var testlist = ["a", "b", "c", "d"];
 verghostjs.vgsidebar = {
 	"": null,
-	"vlc-lua-docs": {
-		"_": [
-			["m", "Module List"],
-			["t", "Type List"]
-		],
-		"m": [
-			["config", "Configuration"],
-			["dialog", "Dialog"],
-			["equalizer", "Equalizer"],
-			["gettext", "GetText"],
-			["errno", "Errno"],
-			["httpd", "HTTPd"],
-			["io", "I/O"],
-			["msg", "Messages"],
-			["misc", "Miscellaneous"],
-			["net", "Network"],
-			["object", "Object"],
-			["osd", "OSD"],
-			["player", "Player"],
-			["playlist", "Playlist"],
-			["rand", "Random"],
-			["rd", "Renderer Discover"],
-			["sd", "Services Discovery"],
-			["stream", "Stream"],
-			["strings", "Strings"],
-			["var", "Variables"],
-			["video", "Video"],
-			["vlm", "VLM"],
-			["volume", "Volume"],
-			["win", "Windows"],
-			["xml", "XML"]
-		],
-		"t": []
-	}
+	"vlc-lua-docs": []
 };
 
 var style_bodge = (e) => {
-	let p_class = dqa("code > span.p");
+	let p_class = dqa(".language-lua code > span.p");
 	if(!p_class) return;
 	for(let i = 0; i < p_class.length; i++) {
 		let el = p_class[i];
